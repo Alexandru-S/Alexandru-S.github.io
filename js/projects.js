75bc909468e14758c5ff6c2d9761313f57d70cd8
Alexandru-S <suleaa@tcd.ie>
2017-11-20 04:28:19 +0000

    fixed errors

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@
 function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", https://api.github.com/users/Alexandru-S/repos, true); // false for synchronous request
+    xmlHttp.open( "GET", "https://api.github.com/users/Alexandru-S/repos", true); // false for synchronous request
     xmlHttp.send( null );
     console.log(xmlHttp);
     return xmlHttp.responseText;
