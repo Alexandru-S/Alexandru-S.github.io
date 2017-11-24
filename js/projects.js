32b22647cd5aa0fabfb85cce699e1143d098f3f9
Alexandru-S <suleaa@tcd.ie>
2017-11-24 01:30:05 +0000

    fetching of repository name is working fully

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,6 @@
 //https://api.github.com/users/Alexandru-S/repos
-$.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json ) {
-  console.log( "JSON Data: " + json );
+$.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json )
+ {
+  console.log( "JSON Data: " + json[0].full_name );
+   $('.projects').append($("<div id='test111'> "+json[0].full_name+  "</div>"));
  });
