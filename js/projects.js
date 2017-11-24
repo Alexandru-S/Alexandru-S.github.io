88bbeedf61aa263b323e9080d71319fcc1b4ed6b
Alexandru-S <suleaa@tcd.ie>
2017-11-24 00:41:44 +0000

    fixed get script so now repositories got to terminal

--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,4 @@
 //https://api.github.com/users/Alexandru-S/repos
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://api.github.com/users/Alexandru-S/repos", true); // false for synchronous request
-    xmlHttp.send( null );
-    console.log(xmlHttp);
-    return xmlHttp.responseText;
-}
+$.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json ) {
+  console.log( "JSON Data: " + json );
+ });
