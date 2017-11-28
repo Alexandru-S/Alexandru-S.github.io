22a46b37e809469f072da08cee577b65febd47f8
Alexandru-S <suleaa@tcd.ie>
2017-11-28 23:43:39 +0000

    removed extraneous code

--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,6 @@ $.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json )
  {
   //console.log( "JSON Data: " + json[0].full_name );
    $('.projects').append($("<div id='test111'> "+json[0].full_name+  "</div>"));
-    $('.projects').append($("<div id='test111'> "+json[1].full_name+  "</div>"));
-     $('.projects').append($("<div id='test111'> "+json[2].full_name+  "</div>"));
-      $('.projects').append($("<div id='test111'> "+json[3].full_name+  "</div>"));
+
+
  });
