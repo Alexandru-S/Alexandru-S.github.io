eee6d0da6a5a395c9c017a456fae3b8cb05b3cc2
Alexandru-S <suleaa@tcd.ie>
2017-11-29 13:41:47 +0000

    fixed get for github

--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,12 @@
 //https://api.github.com/users/Alexandru-S/repos
 $.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json )
  {
-  //console.log( "JSON Data: " + json[0].full_name );
-   $('.projects').append($("<div id='test111'> "+json[0].full_name+  "</div>"));
+  console.log( "JSON Data: " + typeof(json) );
+  var i =0
 
+for(i=0; i<json.length; i++)
+  {
+         $('.projects').append($("<div id='test111'> "+json[i].full_name+  "</div>"));
+    }
 
  });
