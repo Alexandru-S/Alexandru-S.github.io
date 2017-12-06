471ad2a5ef75f3d4ca78e326cf6a5784fc20f894
Alexandru-S <suleaa@tcd.ie>
2017-12-06 23:45:03 +0000

    added comments

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,7 @@
 //https://api.github.com/users/Alexandru-S/repos
 $.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json )
  {
-  console.log( "JSON Data: " + typeof(json) );
+  //console.log( "JSON Data: " + typeof(json) );
   var i =0
 
 for(i=0; i<json.length; i++)
