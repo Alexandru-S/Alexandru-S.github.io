34f177405a1e43bb9ab03493e11f85dfe51f5421
Alexandru-S <suleaa@tcd.ie>
2017-12-14 20:26:44 +0000

    added dynamic picture changing

--- js/projects.js
+++ js/projects.js
@@ -3,10 +3,30 @@ $.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json )
  {
   console.log( "JSON Data: " + typeof(json) );
   var i =0
+  var y = fetchPicture()
 
 for(i=0; i<json.length; i++)
   {
-         $('.w3-row-padding').append($("<div id='project_"+i+"' class='w3-col s6'> "+json[i].full_name+  "<img src='img/sheep.jpg'></div>"));
+         $('.w3-row-padding').append($(
+         "<div id='project_"+i+"' class='w3-col s6'><p> "+json[i].name+  "</p><p> "+json[i].language+  "</p><img src='img/"+fetchPicture(i)+".jpg'>"
+         +""
+         +"<br><br><br></div>"));
 
     }
  });
+
+
+var x = fetchPicture(4, 3);
+
+function fetchPicture(picture) {
+    var k = "sheep"
+    var j = "mtns1"
+    if(picture ==1)
+    {
+        return j;
+    }
+    if(picture !=1)
+    {
+        return k;
+    }
+}
