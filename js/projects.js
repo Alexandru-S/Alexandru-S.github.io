08ef288a54329183ddb626a71ca2a7b7c314c400
Alexandru-S <suleaa@tcd.ie>
2017-12-14 19:36:10 +0000

    added the gridview spacing

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,12 @@
 //https://api.github.com/users/Alexandru-S/repos
 $.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json )
  {
-  //console.log( "JSON Data: " + typeof(json) );
+  console.log( "JSON Data: " + typeof(json) );
   var i =0
 
 for(i=0; i<json.length; i++)
   {
-         $('.projects').append($("<div id='project_"+i+"' class='w3-col s6'> "+json[i].full_name+  "<img src='img/sheep.jpg'></div>"));
+         $('.w3-row-padding').append($("<div id='project_"+i+"' class='w3-col s6'> "+json[i].full_name+  "<img src='img/sheep.jpg'></div>"));
 
     }
  });
