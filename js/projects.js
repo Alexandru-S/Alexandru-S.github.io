3b61225499f16636817811aa1991c1697fecad23
Alexandru-S <suleaa@tcd.ie>
2018-02-23 03:32:09 +0000

    refactored website to mdl standards, have to refurbish contact and blog page

--- js/projects.js
+++ js/projects.js
@@ -1,15 +1,28 @@
 //https://api.github.com/users/Alexandru-S/repos
 $.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json )
  {
-  //console.log( "JSON Data: " + typeof(json) );
+  console.log( "JSON Data: " + typeof(json) );
   var i =0
   var y = fetchPicture()
 
 for(i=0; i<json.length; i++)
   {
-         $('.w3-row-padding').append($(
-         "<div id='project_"+i+"' class='w3-col s6'><h1> "+json[i].name+  "</h1><p> "+json[i].language+  "</p>  <a href='"+json[i].html_url +"'><img src='img/"+fetchPicture(json[i].language)+".jpg'></a>"
-         +"<br><br><br></div>"));
+         $('.mdl-grid').append($(
+         "<div id='project_"+i+"' class='mdl-cell mdl-card mdl-shadow--4dp portfolio-card'>"+
+         "<div class='mdl-card__media'>"+
+            "<img class='article-image' src='img/"+fetchPicture(json[i].language)+".jpg' border='0' alt=''>"+
+         "</div>"+
+         "<div class='mdl-card__title'>"+
+            "<h2 class='mdl-card__title-text'>"+json[i].name+"</h2>"+
+         "</div>"+
+         "<div class='mdl-card__supporting-text'>"+
+               "<p> "+json[i].language+  "</p>"+
+         "</div>"+
+
+         "<div class='mdl-card__actions mdl-card--border'>"+
+               "<a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent' href= href='"+json[i].html_url +"'>Visit</a>"+
+         "</div>"+
+         "</div>"));
 
     }
  });
