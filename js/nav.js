3eed23f69c42ffdd08a2f3e70e16a106a58302e1
Alexandru-S <suleaa@tcd.ie>
2019-02-11 19:08:46 +0000

    refactored entire website to run off materializecss instead of getmdl due to getmdl having stopped support

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,5 @@
-$(document).ready(function()
-{
-
-	(function($) {
-
-		$('#header__icon').click(function(e){
-			e.preventDefault();
-			$('body').toggleClass('with--sidebar');
-		});
-
-    $('#site-cache').click(function(e){
-      $('body').removeClass('with--sidebar');
-    });
-
-	})(jQuery);
-
+var options = {}
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
 });
-
--- js/projects.js
+++ js/projects.js
@@ -1,58 +1,48 @@
 //https://api.github.com/users/Alexandru-S/repos
 $.getJSON( "https://api.github.com/users/Alexandru-S/repos", function( json )
  {
-  console.log( "JSON Data: " + typeof(json) );
   var i =0
   var y = fetchPicture()
 
-for(i=0; i<json.length; i++)
-  {
-         $('.mdl-grid').append($(
-         "<div id='project_"+i+"' class='mdl-cell mdl-card mdl-shadow--4dp portfolio-card'>"+
-         "<div class='mdl-card__media'>"+
-            "<img class='article-image' src='img/"+fetchPicture(json[i].language)+".jpg' border='0' alt=''>"+
-         "</div>"+
-         "<div class='mdl-card__title'>"+
-            "<h2 class='mdl-card__title-text'>"+json[i].name+"</h2>"+
-         "</div>"+
-         "<div class='mdl-card__supporting-text'>"+
-               "<p> "+json[i].language+  "</p>"+
-         "</div>"+
-
-         "<div class='mdl-card__actions mdl-card--border'>"+
-               "<a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent' href= href='"+json[i].html_url +"'>Visit</a>"+
-         "</div>"+
+for(i=0; i<json.length; i++){
+         $('.row').append($(
+         "<div id='project_"+i+"' class='col s12 m6 portfolio-card'>"+
+            "<div class='card'>"+
+                 "<div class='card-image'>"+
+                    "<img class='article-image' src='img/"+fetchPicture(json[i].language)+".jpg' border='0' alt=''>"+
+                    "<span class='card-title'>"+json[i].name+"</span>"+
+                 "</div>"+
+                 "<div class='card-content'>"+
+                       "<p> "+json[i].language+  "</p>"+
+                 "</div>"+
+                 "<div class='card-action'>"+
+                       "<a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent' href= href='"+json[i].html_url +"'>Visit</a>"+
+                 "</div>"+
+            "</div>"+
          "</div>"));
-
     }
  });
 
-
 function fetchPicture(picture) {
-
-    console.log( "JSON Data: " + picture );
     switch(picture) {
-            case "HTML":
-                return "html"
-                 break;
-            case "C++":
-                return "cplus"
-                 break;
-            case "C":
-                return "c"
-                 break;
+        case "HTML":
+            return "html"
+             break;
+        case "C++":
+            return "cplus"
+             break;
+        case "C":
+            return "c"
+             break;
         case "TeX":
             return "tex"
              break;
         case "Java":
              return "java"
              break;
-
         case "Python":
             return "python"
             break;
-
-
         default:
             return "sheep"
     }
