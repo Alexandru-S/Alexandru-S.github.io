0fe7d49a2966abb98e6e5190e50ee67cf5580387
Alexandru-S <alex.sulea@gmail.com>
2019-09-09 00:11:46 +0100

    added working dynamic paralax pictures

--- js/parallax_image.js
+++ js/parallax_image.js
@@ -1,18 +1,11 @@
-console.log("------- paralalx image section")
-
 var APIKEY = 'd6e1fdf795a9d10b971c04b8f02097745689cbc1ee15ac8b9156be9f540c984c'
 $.getJSON( "https://api.unsplash.com/search/photos?query=chicago&client_id=d6e1fdf795a9d10b971c04b8f02097745689cbc1ee15ac8b9156be9f540c984c", function( json ){
-    console.log("json stuff parallax image coord here",json );
-    var imageList = json.results
-
-    $.each(imageList,funtion(i,val){
+    var imageList = json.results;
+    $.each(imageList,function(i,val){
         var image = val;
         var imageURL = val.urls.regular;
         var imageWidth = val.width;
         var imageHeight = val.height;
-
-
-
+        document.getElementById('image_parallax').src =imageURL
     })
-
 });
